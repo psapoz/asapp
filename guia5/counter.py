d66e4ec8aec5d26ff0c81f71d6a3a40ee30eb930
Paola Sapoznik <32316483+psapoz@users.noreply.github.com>
2020-10-27 01:13:07 -0300

    Update counter.py

--- guia5/counter.py
+++ guia5/counter.py
@@ -1,84 +1,54 @@
 import os.path
+import sys
 
 FN = 'counter.txt'
 
 
-class Action:
-    def __init__(self, value):
-        self._value = value
-
-    def exec(self):
-        print(self.__str__())
-
-
-class Inc(Action):
-    __msg = 'Se incremento'
-
-    def __str__(self):
-        return Inc.__msg
-
-    def exec(self):
-        super().exec()
-        return self._value + 1
-
-
-class Dec(Action):
-    __msg = 'Se Decremento`'
-
-    def __str__(self):
-        return Dec.__msg
-
-    def exec(self):
-        super().exec()
-        return self._value - 1
-
-
 class Counter:
     __msg = 'Contador -> {}'
+    __actions = {
+        'inc': (1, 'Se incrementó'),
+        'dec': (-1, 'Se decrementó')
+    }
 
-    def __init__(self, action=None):
+    def __init__(self):
         self.__value = 0
         self.__load()
-        if action:
-            self.__update_file(action)
 
     def __str__(self):
         return Counter.__msg.format(self.__value)
+
+    def update(self, arg):
+        action = arg.lower()
+        if action in Counter.__actions.keys():
+            self.__value += Counter.__actions[action][0]
+            print(Counter.__actions[action][1])
+            self.__store()
 
     def __load(self):
         mode = 'r+' if os.path.exists(FN) else 'w'
 
         with open(FN, mode) as f:
-            value_from_file = f.readline().replace('\n', '')
+            value_from_file = f.read().rstrip()
 
             if value_from_file:
                 self.__value = int(value_from_file)
             else:
                 f.write(str(self.__value))
 
-    def __update_file(self, action):
+    def __store(self):
         with open(FN, 'w+') as f:
-            try:
-                self.__value = action(self.__value).exec()
-            except TypeError:
-                pass
-            finally:
-                f.seek(0)
-                f.write(str(self.__value))
+            f.seek(0)
+            f.write(str(self.__value))
 
 
 def main():
-    cont3 = Counter('bla')
-    print(cont3)
+    counter = Counter()
 
-    cont0 = Counter(Dec)
-    print(cont0)
+    if len(sys.argv) == 2:
+        counter.update(sys.argv[1])
 
-    cont1 = Counter()
-    print(cont1)
-
-    cont2 = Counter(Inc)
-    print(cont2)
+    print(counter)
 
 
 if __name__ == '__main__':
